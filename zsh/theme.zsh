538c46dd466c7b0bbe9dde81fe30931fd9fdb93b
Thanushen Balaskandar <thanushenbalaskandar@gmail.com>
2023-02-16 18:10:47 +0000

    first commit

---
+++ zsh/theme.zsh
@@ -0,0 +1,40 @@
+##
+## Aesthetic Night Colorscheme
+##
+# special
+background='#1E1E2E'
+foreground='#CDD6F4'
+
+# black
+color0='#282a2e'
+color8='#373b41'
+
+# red
+color1='#9800ff'
+color9='#c165ff'
+
+# green
+color2='#9b2c82'
+color10='#de59c0'
+
+# yellow
+color3='#f33482'
+color11='#f074c2'
+
+# blue
+color4='#e110a5'
+color12='#c566a8'
+
+# magenta
+color5='#a2258c'
+color13='#f278db'
+
+# cyan
+color6='#a12bec'
+color14='#d68df3'
+
+# white
+color7='#707880'
+color15='#c5c8c6'
+
+# vim='iletype'=zsh
